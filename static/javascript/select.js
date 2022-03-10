0d0fdc87fff4851e008884518b869d32d01df562
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2022-03-10 20:16:23 +0100

    Preparation for the copyToClipboard function

--- static/javascript/select.js
+++ static/javascript/select.js
@@ -1,6 +1,12 @@
 const btns = document.querySelectorAll(".btn");
+const pres = document.querySelectorAll(".pre");
+
 for (const btn of btns) {
   btn.addEventListener("click", function () {
     window.open("https://kahboom.tk/quizID/" + btn.name, "_self");
   });
+}
+
+function copyToClipboard(IDToCopy) {
+  // ADD COPY CLIPBOARD FUNCTION
 }
